db19ebfc1c858b89894b996b6a6f0d9f5a8aeaa3
Samet Orbuk <orbuk60@gmail.com>
2024-09-21 10:00:12 +0300

    s13 challenge

--- src/employeeApp/Plan.java
+++ src/employeeApp/Plan.java
@@ -5,4 +5,31 @@ public enum Plan {;
     private  String name;
     private  double price;
 
+
+    Plan(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String toString(){
+        return "Plan:{"+
+                "name:"+ name +
+                "price:"+ price;
+    }
 }
